29dd0fa78c3e1d1dad8b2d28b1529c328c08a9e1
HJFTM <homejularic@gmail.com>
2025-06-19 19:31:51 +0200

    Update observablehq.config.js

--- hello-framework/observablehq.config.js
+++ hello-framework/observablehq.config.js
@@ -26,21 +26,38 @@ export default {
       pages: [
         {name: "Prezime Jular-ić (K)", path: "/pages/1_Jularic/01.1.prezime_ilaric"},
         {name: "Evolucija zapisa (V)", path: "/pages/1_Jularic/01.2.evolucija"},
-        {name: "Prezime (E)",          path: "/pages/1_Jularic/01.3.prezime_e"}
+        {name: "Prezime (E)",          path: "/pages/1_Jularic/01.3.prezime_e"},
+        {name: "Generacije (K)",       path: "/pages/2 Generacije/Generacije_K"},
+        {name: "Obitelji (K)",         path: "/pages/2 Obitelji/Obitelji_K"}, 
+        {name: "Migracije (K)",        path: "/pages/9 Migracije/Migracije_K"}, 
+        {name: "Mjesta (K)",           path: "/pages/10 Mjesta/Mjesta_K"},  
+        {name: "Kuće (K)",             path: "/pages/11 Kucedomacin/Kucedomacin_K"},
+        {name: "Popisi (K)",           path: "/pages/15 Popisi/Popisi_K"},
+        {name: "Župe (K)",             path: "/pages/20 Zupe/Zupe_K"}, 
+        {name: "Izvori (K)",           path: "/pages/21 Izvori/Izvori_K"}, 
+        {name: "Države (K)",           path: "/pages/30 Drzava/Drzava_K"},
+        {name: "Katastar (K)",         path: "/pages/31 Katastar/Katastar_K"},
+        {name: "Pismo (K)",            path: "/pages/40 Pismo/Pismo_K"}, 
+        {name: "Bolesti (K)",          path: "/pages/70 Bolesti/Bolesti_K"},
+        {name: "Groblje (K)",          path: "/pages/71 Groblje/Groblje_K"}
       ]
     }, 
     {
-      name: "Generacije",
+      name: "Bosna",
       pages: [
-        {name: "Generacije (K)", path: "/pages/2 Generacije/Generacije_K"},
-        {name: "Generacije (R)", path: "/pages/2 Generacije/Generacije_R"}
+
+        {name: "Generacije (R)",      path: "/pages/2 Generacije/Generacije_R"}
+        {name: "Obitelji (R)",        path: "/pages/2 Obitelji/Obitelji_R"},
+        {name: "Migracije (R)",       path: "/pages/9 Migracije/Migracije_R"},   
+        {name: "Mjesta (R)",          path: "/pages/10 Mjesta/Mjesta_R"},
+        {name: "Župe (R)",           path: "/pages/20 Zupe/Zupe_D"},        
       ]
     } , 
     {
       name: "Obitelji",
       pages: [
-        {name: "Obitelji (K)", path: "/pages/2 Obitelji/Obitelji_K"},
-        {name: "Obitelji (R)", path: "/pages/2 Obitelji/Obitelji_R"},
+
+
         {name: "Obitelji (E)", path: "/pages/2 Obitelji/Obitelji_E"},
         {name: "Obitelji (Z)", path: "/pages/2 Obitelji/Obitelj_Zapisi"}
       ]
@@ -49,8 +66,8 @@ export default {
     {
       name: "Migracije",
       pages: [
-        {name: "Migracije (K)", path: "/pages/9 Migracije/Migracije_K"},
-        {name: "Migracije (R)", path: "/pages/9 Migracije/Migracije_R"},
+
+
         {name: "Migracije Individualne (E)", path: "/pages/9 Migracije/Migracije_Individualne_E"},
         {name: "Migracije Masovne (E)", path: "/pages/9 Migracije/Migracije_Masovne_E"}
       ]
@@ -58,22 +75,22 @@ export default {
     {
       name: "Mjesta",
       pages: [
-        {name: "Mjesta (K)", path: "/pages/10 Mjesta/Mjesta_K"},
-        {name: "Mjesta (R)", path: "/pages/10 Mjesta/Mjesta_R"},
+
+
         {name: "Mjesta (E)", path: "/pages/10 Mjesta/Mjesta_E"}
       ]
     } ,    
     {
       name: "Kuće",
       pages: [
-        {name: "Kuće (K)", path: "/pages/11 Kucedomacin/Kucedomacin_K"},
+
         {name: "Kuće (E)", path: "/pages/11 Kucedomacin/Kucedomacin_E"}
       ]
     } ,  
     {
       name: "Popisi",
       pages: [
-        {name: "Popisi (K)", path: "/pages/15 Popisi/Popisi_K"},
+
         {name: "Popisi (D)", path: "/pages/15 Popisi/Popisi_D"},
         {name: "Popisi (E)", path: "/pages/15 Popisi/Popisi_E"}
       ]
@@ -81,15 +98,15 @@ export default {
      {
       name: "Zupe",
       pages: [
-        {name: "Župe (K)", path: "/pages/20 Zupe/Zupe_K"},
-        {name: "Župe (R)", path: "/pages/20 Zupe/Zupe_D"},
+
+
         {name: "Župe (E)", path: "/pages/20 Zupe/Zupe_E"}
       ]
     } ,  
     {
       name: "Izvori",
       pages: [
-        {name: "Izvori (K)", path: "/pages/21 Izvori/Izvori_K"},
+
         {name: "Izvori (D)", path: "/pages/21 Izvori/Izvori_D"},
         {name: "Izvori (E)", path: "/pages/21 Izvori/Izvori_E"}
       ]
@@ -97,14 +114,14 @@ export default {
     {
       name: "Države",
       pages: [
-        {name: "Države (K)", path: "/pages/30 Drzava/Drzava_K"},
+       
         {name: "Države (E)", path: "/pages/30 Drzava/Drzava_E"}
       ]
     } , 
     {
       name: "Katastar",
       pages: [
-        {name: "Katastar (K)", path: "/pages/31 Katastar/Katastar_K"},
+
         {name: "Katastar (D)", path: "/pages/31 Katastar/Katastar_D"},
         {name: "Katastar (E)", path: "/pages/31 Katastar/Katastar_E"}
       ]
@@ -112,7 +129,7 @@ export default {
     {
       name: "Pismo",
       pages: [
-        {name: "Pismo (K)", path: "/pages/40 Pismo/Pismo_K"},
+
         {name: "Pismo (D)", path: "/pages/40 Pismo/Pismo_D"},
         {name: "Pismo (E)", path: "/pages/40 Pismo/Pismo_E"}
       ]
@@ -120,7 +137,7 @@ export default {
     {
       name: "Bolesti",
       pages: [
-        {name: "Bolesti (K)", path: "/pages/70 Bolesti/Bolesti_K"},
+
         {name: "Bolesti (D)", path: "/pages/70 Bolesti/Bolesti_D"},
         {name: "Bolesti (E)", path: "/pages/70 Bolesti/Bolesti_E"}
       ]
@@ -128,7 +145,7 @@ export default {
     {
       name: "Groblje",
       pages: [
-        {name: "Groblje (K)", path: "/pages/71 Groblje/Groblje_K"},
+
         {name: "Groblje (D)", path: "/pages/71 Groblje/Groblje_D"},
         {name: "Groblje (E)", path: "/pages/71 Groblje/Groblje_E"}
       ]
