6db8c60f7090c5b571e0cdb6a37dae28e6e5b577
HJFTM <homejularic@gmail.com>
2025-06-27 17:11:02 +0200

    Update observablehq.config.js

--- hello-framework/observablehq.config.js
+++ hello-framework/observablehq.config.js
@@ -52,7 +52,7 @@ export default {
         {name: "Mjesta (R)",          path: "/pages/ROD/Mjesta_R"},
         {name: "Župe (R)",            path: "/pages/ROD/Zupe_D"}, 
         {name: "Župe Rodos. (R)",     path: "/pages/ROD/Zupe_rodoslovlje_R"}, 
-        {name: "Župe Rodos. (R)",     path: "/pages/ROD/Zupe_obitelji_R"}, 
+        {name: "Župe Obitelji (R)",     path: "/pages/ROD/Zupe_obitelji_R"}, 
         
         {name: "Izvori* (D)",         path: "/pages/ROD/Popisi_D"},   
         {name: "Pismo (D)",           path: "/pages/ROD/Pismo_D"},
