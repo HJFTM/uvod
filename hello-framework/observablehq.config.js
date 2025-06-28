f1758532e3b05594d385245f1377caa1725d56a9
HJFTM <homejularic@gmail.com>
2025-06-28 13:49:29 +0200

    Update observablehq.config.js

--- hello-framework/observablehq.config.js
+++ hello-framework/observablehq.config.js
@@ -70,7 +70,10 @@ export default {
     {
       name: "Entiteti",
       pages: [
+        {name: "1700. Marko", path: "/pages/ENTITET/Obitelji_E?OBITELJ=1700.%20Marko"},
+        
         {name: "Obitelji (E)", path: "/pages/ENTITET/Obitelji_E"},
+        
         {name: "Obitelji (Z)", path: "/pages/ENTITET/Obitelj_Zapisi"},
         {name: "Zapisi (E)",   path: "/pages/ENTITET/Zapisi_E"},        
         {name: "Mig. Ind. (E)", path: "/pages/ENTITET/Migracije_Individualne_E"},
