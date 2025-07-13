4f6cc6c3c69f79ab4681074ef19120086bfb87c8
HJFTM <homejularic@gmail.com>
2025-07-13 16:46:33 +0200

    Update observablehq.config.js

--- hello-framework/observablehq.config.js
+++ hello-framework/observablehq.config.js
@@ -193,7 +193,7 @@ const pages = (CURRENT_PROJECT === 'svi_rodovi')
   : rodEntitetiIzvoriPages;
 
 export default {
-  title: "Jularić",
+  title: `Jularić (${CURRENT_PROJECT})`,
   pages,
   dynamicPaths: () => {
     // ovdje možeš staviti logiku ako treba
