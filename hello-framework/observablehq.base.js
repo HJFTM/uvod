6e0a8f4048a232ec2bd20bbe3b8cdec521e2f20f
HJFTM <homejularic@gmail.com>
2025-07-15 22:23:06 +0200

    Update observablehq.base.js

--- hello-framework/observablehq.base.js
+++ hello-framework/observablehq.base.js
@@ -25,21 +25,30 @@ export function generirajObiteljiPoMjestu(data, rod) {
   }));
 }
 
-export function generirajMjestaOdObitelji(data, rod) {
-  if (!rod) rod = "Bosna";
-  const mjestaMap = new Map();
-  for (const o of data) {
-    if (!o.ROD || o.ROD !== rod || !o.MJESTO || !o.OBITELJ || !o.GODINA) continue;
-    const mjesto = o.MJESTO.trim();
-    const godina = parseInt(o.GODINA);
-    if (!mjestaMap.has(mjesto) || godina < mjestaMap.get(mjesto)) {
-      mjestaMap.set(mjesto, godina);
+function generirajMjestaOdObitelji(obitelji, rod) {
+    if (rod == null) rod = "Bosna";
+    const mjestaMap = new Map();
+    const obitelj_m = obitelji.filter(o => o.TIP == "M");
+    for (const o of obitelj_m) {
+        if (!o.ROD || o.ROD !== rod || !o.MJESTO || !o.OBITELJ || !o.GODINA) continue;
+        const mjesto = o.MJESTO.trim();
+        const godina = parseInt(o.GODINA);
+
+        if (!mjestaMap.has(mjesto)) {
+            mjestaMap.set(mjesto, godina);
+        } else {
+            const postojecaGodina = mjestaMap.get(mjesto);
+            if (godina < postojecaGodina) {
+                mjestaMap.set(mjesto, godina);
+            }
+        }
     }
-  }
-  return Array.from(mjestaMap.entries())
-    .sort((a, b) => a[1] - b[1])
-    .map(([mjesto, godina]) => ({
-      name: `${godina}. ${mjesto}`,
-      path: `/pages/ENTITET/mjesto/${encodeURIComponent(mjesto)}`
-    }));
+
+    return Array.from(mjestaMap.entries())
+        .sort((a, b) => a[1] - b[1])  // sortiraj po godini (a[1] je godina)
+        .map(([mjesto, godina]) => ({
+            name: `${godina}. ${mjesto}`,
+            path: `/pages/ENTITET/mjesto/${encodeURIComponent(mjesto)}`
+        }));
 }
+
