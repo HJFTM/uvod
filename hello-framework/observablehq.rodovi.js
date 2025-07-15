f9d078ccc4e7e076f368cf90e6715e1da7b67b8e
HJFTM <homejularic@gmail.com>
2025-07-15 21:50:52 +0200

    Create observablehq.rodovi.js

---
+++ hello-framework/observablehq.rodovi.js
@@ -0,0 +1,23 @@
+const obiteljiPoMjestuPages = generirajObiteljiPoMjestu(data, CURRENT_PROJECT);
+const mjestaPages = generirajMjestaOdObitelji(data, CURRENT_PROJECT);
+
+export const rodEntitetiIzvoriPages = [
+  {
+    name: `Rod (${CURRENT_PROJECT})`,
+    pages: [
+      { name: "Prezime", path: `/pages/1_Jularic/prezime_r?ROD=${encodeURIComponent(CURRENT_PROJECT)}` },
+      { name: "Generacije", path: `/pages/ROD/Generacije_R?ROD=${encodeURIComponent(CURRENT_PROJECT)}` },
+      { name: "Obitelji", path: `/pages/ROD/Obitelji_R?ROD=${encodeURIComponent(CURRENT_PROJECT)}` },
+      { name: "Stablo", path: `/pages/ROD/Stablo_R?ROD=${encodeURIComponent(CURRENT_PROJECT)}` },
+    ]
+  },
+  {
+    name: "Mjesta",
+    pages: [
+      { name: "Mjesta", path: `/pages/ROD/Mjesta_R?ROD=${encodeURIComponent(CURRENT_PROJECT)}` },
+      { name: "Mjesta-zapisi", path: `/pages/KONCEPT/Mjesta_zapisi_R?ROD=${encodeURIComponent(CURRENT_PROJECT)}` },
+      ...mjestaPages,
+    ]
+  },
+  ...obiteljiPoMjestuPages
+];
