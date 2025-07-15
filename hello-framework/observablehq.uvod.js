a0e838d1f09a6c1ef1b53576f11fc455f5dd9d22
HJFTM <homejularic@gmail.com>
2025-07-15 21:51:24 +0200

    Create observablehq.uvod.js

---
+++ hello-framework/observablehq.uvod.js
@@ -0,0 +1,56 @@
+export const uvodPages = [
+  {
+    name: "Uvod",
+    pages: [
+      { name: "Naslovnica", path: "/pages/0 Uvod/naslovnica" },
+      { name: "Uvod", path: "/pages/0 Uvod/uvod" },
+      { name: "Događaji", path: "/pages/0 Uvod/dogadjaji" }
+    ]
+  },
+  {
+    name: "Prezime",
+    pages: [
+      { name: "Prezime Jular-ić", path: "/pages/1_Jularic/01.1.prezime_ilaric" },
+      { name: "Evolucija zapisa", path: "/pages/1_Jularic/01.2.evolucija" },
+      { name: "Slična prezimena", path: "/pages/1_Prezime/prezime_slicno" },
+    ]
+  },
+  {
+    name: "Rodovi",
+    pages: [
+      { name: "Države", path: "/pages/KONCEPT/Drzava_K" },
+      { name: "Župe", path: "/pages/KONCEPT/Zupe_K" },
+      { name: "Mjesta", path: "/pages/KONCEPT/Mjesta_K" },
+    ]
+  },
+  {
+    name: "Obitelji",
+    pages: [
+      { name: "Generacije", path: "/pages/KONCEPT/Generacije_K" },
+      { name: "Obitelji", path: "/pages/KONCEPT/Obitelji_K" },
+      { name: "Brakovi", path: "/pages/KONCEPT/Brakovi_K" },
+      { name: "Povezane obitelji", path: "/pages/KONCEPT/Obitelji_povezane_K" },
+      { name: "Događaji - zapisi", path: "/pages/KONCEPT/Zapisi_K" },
+      { name: "Bolesti", path: "/pages/KONCEPT/Bolesti_K" },
+      { name: "Groblja", path: "/pages/KONCEPT/Groblje_K" },
+    ]
+  },
+  {
+    name: "Migracije",
+    pages: [
+      { name: "Individualne", path: "/pages/KONCEPT/Migracije_K" },
+      { name: "Masovne", path: "/pages/KONCEPT/Migracije_masovne_K" },
+    ]
+  },
+  {
+    name: "Izvori",
+    pages: [
+      { name: "Popisi*", path: "/pages/KONCEPT/Popisi_K" },
+      { name: "Izvori", path: "/pages/KONCEPT/Izvori_K" },
+      { name: "Popisi kuća", path: "/pages/KONCEPT/Kucedomacin_K" },
+      { name: "Popisi stanovnika", path: "/pages/KONCEPT/Popis_stanovnika_K" },
+      { name: "Katastar (K)", path: "/pages/KONCEPT/Katastar_K" },
+      { name: "Pismo", path: "/pages/KONCEPT/Pismo_K" },
+    ]
+  }
+];
