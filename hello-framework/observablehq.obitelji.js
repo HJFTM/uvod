a85c65ab11328499ef478c28a080a8c552951ece
HJFTM <homejularic@gmail.com>
2025-07-16 19:00:56 +0200

    Update observablehq.obitelji.js

--- hello-framework/observablehq.obitelji.js
+++ hello-framework/observablehq.obitelji.js
@@ -1,9 +1,14 @@
 
 import { CURRENT_PROJECT, data, generirajObiteljiPoMjestu, generirajMjestaOdObitelji } from "./observablehq.base.js";
 
-const obiteljiPoMjestuPages = generirajObiteljiPoMjestu(data, "Bosna");
+const obiteljiBH = generirajObiteljiPoMjestu(data, "Bosna");
+const obiteljiST = generirajObiteljiPoMjestu(data, "Stupnik");
+const obiteljiDU = generirajObiteljiPoMjestu(data, "Dubrovnik");
+
 const mjestaPages = generirajMjestaOdObitelji(data, CURRENT_PROJECT);
 
 export const obiteljiPages = [
-  ...obiteljiPoMjestuPages
+  ...obiteljiBH,
+  ...obiteljiST,
+  ...obiteljiDU,  
 ];
