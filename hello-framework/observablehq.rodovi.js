6af39f55372c1ac55bd5a4eda69fb8ddaea1afc9
HJFTM <homejularic@gmail.com>
2025-07-17 15:08:35 +0200

    Update observablehq.rodovi.js

--- hello-framework/observablehq.rodovi.js
+++ hello-framework/observablehq.rodovi.js
@@ -5,17 +5,18 @@ const mjestaPages = generirajMjestaOdObitelji(data, CURRENT_PROJECT);
 
 export const rodEntitetiIzvoriPages = [
   {
-    name: `Rod ➔ Obitelji`,
+    name: `Obitelji`,
     pages: [
       { name: "Prezime",      path: `/pages/1_Jularic/prezime_r?ROD=${encodeURIComponent(CURRENT_PROJECT)}` },
       { name: "Generacije",   path: `/pages/ROD/Generacije_R?ROD=${encodeURIComponent(CURRENT_PROJECT)}` },
       { name: "Obitelji",     path: `/pages/ROD/Obitelji_R?ROD=${encodeURIComponent(CURRENT_PROJECT)}` },
       { name: "Događaji",     path: `/pages/ROD/Dogadjaji_R?ROD=${encodeURIComponent(CURRENT_PROJECT)}` },      
       { name: "Stablo",       path: `/pages/ROD/Stablo_R?ROD=${encodeURIComponent(CURRENT_PROJECT)}` },
+      { name: "Bolesti",      path: `/pages/ROD/Bolesti_D?ROD=${encodeURIComponent(CURRENT_PROJECT)}` },
     ]
   },
   {
-    name: "Mjesta ➔ Migracije",
+    name: "Mjesta",
     pages: [
       { name: "Mjesta",          path: `/pages/ROD/Mjesta_R?ROD=${encodeURIComponent(CURRENT_PROJECT)}` },
       //{ name: "Mjesta-zapisi",   path: `/pages/KONCEPT/Mjesta_zapisi_R?ROD=${encodeURIComponent(CURRENT_PROJECT)}` },
