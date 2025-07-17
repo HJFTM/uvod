255af5bacd817b50a0b2c896317fbad57918cc4d
HJFTM <homejularic@gmail.com>
2025-07-17 11:02:24 +0200

    Update observablehq.rodovi.js

--- hello-framework/observablehq.rodovi.js
+++ hello-framework/observablehq.rodovi.js
@@ -29,7 +29,9 @@ export const rodEntitetiIzvoriPages = [
       { name: "Izvori",           path: `/pages/ROD/Izvori_D?ROD=${encodeURIComponent(CURRENT_PROJECT)}` },      
       { name: "Popisi*",          path: `/pages/ROD/Popisi_D?ROD=${encodeURIComponent(CURRENT_PROJECT)}` },
       { name: "Katastar",         path: `/pages/ROD/Katastar_D?ROD=${encodeURIComponent(CURRENT_PROJECT)}` }, 
-      { name: "Groblje",          path: `/pages/ROD/Groblje_D?ROD=${encodeURIComponent(CURRENT_PROJECT)}` },      
+      { name: "Groblje",          path: `/pages/ROD/Groblje_D?ROD=${encodeURIComponent(CURRENT_PROJECT)}` },  
+      { name: "Pismo / Jezik",    path: `/pages/ROD/Pismo_D?ROD=${encodeURIComponent(CURRENT_PROJECT)}` },
+  
     ]
   }
 ];
