50a0c483783755d33c4c4d1ad540041192448894
HJFTM <homejularic@gmail.com>
2025-07-25 09:49:44 +0200

    Update observablehq.uvod.js

--- hello-framework/observablehq.uvod.js
+++ hello-framework/observablehq.uvod.js
@@ -60,6 +60,7 @@ export const uvodPages = [
       { name: "Gradski",   path: "/pages/KONCEPT/Izvori_Gradski" },  
       { name: "Radovi",    path: "/pages/KONCEPT/Izvori_Radovi" },
       { name: "Groblja",   path: "/pages/KONCEPT/Izvori_Groblja" },  
+      { name: "Pismo",            path: "/pages/KONCEPT/Pismo_K" },
        ]
   },
   {
@@ -69,7 +70,6 @@ export const uvodPages = [
       { name: "Kućedomaćini",     path: "/pages/KONCEPT/Kucedomacin_K" },
       { name: "Katastar",         path: "/pages/KONCEPT/Katastar_K" },
       { name: "Stanovnici",       path: "/pages/KONCEPT/Popis_stanovnika_K" },
-      { name: "Pismo",            path: "/pages/KONCEPT/Pismo_K" },
     ]
   },
     {
