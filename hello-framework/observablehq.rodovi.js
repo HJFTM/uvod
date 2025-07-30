71775256b7164b30259f8b4c7a4eb65007209eba
HJFTM <homejularic@gmail.com>
2025-07-30 18:45:39 +0200

    Update observablehq.rodovi.js

--- hello-framework/observablehq.rodovi.js
+++ hello-framework/observablehq.rodovi.js
@@ -1,43 +1,46 @@
-import { CURRENT_PROJECT, data, generirajObiteljiPoMjestu, generirajMjestaOdObitelji } from "./observablehq.base.js";
+import {
+  generirajObiteljiPoMjestu,
+  generirajMjestaOdObitelji
+} from "./observablehq.base.js";
 
-const obiteljiPoMjestuPages = generirajObiteljiPoMjestu(data, CURRENT_PROJECT);
-const mjestaPages = generirajMjestaOdObitelji(data, CURRENT_PROJECT);
+export function getRodEntitetiIzvoriPages(rod, data) {
+  const obiteljiPoMjestuPages = generirajObiteljiPoMjestu(data, rod);
+  const mjestaPages = generirajMjestaOdObitelji(data, rod);
 
-export const rodEntitetiIzvoriPages = [
-  {
-    name: `Obitelji`,
-    pages: [
-      { name: "  Prezime",      path: `/pages/1_Jularic/prezime_r?ROD=${encodeURIComponent(CURRENT_PROJECT)}` },
-      { name: "  Generacije",   path: `/pages/ROD/Generacije_R?ROD=${encodeURIComponent(CURRENT_PROJECT)}` },
-      { name: "  Obitelji",     path: `/pages/ROD/Obitelji_R?ROD=${encodeURIComponent(CURRENT_PROJECT)}` },
-      { name: "  Događaji",     path: `/pages/ROD/Dogadjaji_R?ROD=${encodeURIComponent(CURRENT_PROJECT)}` },      
-      { name: "  Stablo",       path: `/pages/ROD/Stablo_R?ROD=${encodeURIComponent(CURRENT_PROJECT)}` },
-      { name: "  Bolesti",      path: `/pages/ROD/Bolesti_D?ROD=${encodeURIComponent(CURRENT_PROJECT)}` },
-      { name: "  Mjesta",          path: `/pages/ROD/Mjesta_R?ROD=${encodeURIComponent(CURRENT_PROJECT)}` },
-      //{ name: "Mjesta-zapisi",   path: `/pages/KONCEPT/Mjesta_zapisi_R?ROD=${encodeURIComponent(CURRENT_PROJECT)}` },
-      { name: "  Migracije",       path: `/pages/ROD/Migracije_R?ROD=${encodeURIComponent(CURRENT_PROJECT)}` },      
-      { name: "  Izvori*",         path: `/pages/ROD/Izvori_D?ROD=${encodeURIComponent(CURRENT_PROJECT)}` },
-    ]
-  },
-  {
-    name: "Mjesta",
-    pages: [...mjestaPages,]
-  },
-  {
-    name: "Izvori",
-    pages: [      
-      { name: "  Popisi",          path: `/pages/ROD/Izvor_Popisi?ROD=${encodeURIComponent(CURRENT_PROJECT)}` },
-      { name: "  Matice",           path: `/pages/ROD/Matice_D?ROD=${encodeURIComponent(CURRENT_PROJECT)}` },  
-      { name: "  Groblje",          path: `/pages/ROD/Groblje_D?ROD=${encodeURIComponent(CURRENT_PROJECT)}` }, 
-      { name: "  Katastar",         path: `/pages/ROD/Katastar_D?ROD=${encodeURIComponent(CURRENT_PROJECT)}` },
-      { name: "  Pismo / Jezik",    path: `/pages/ROD/Pismo_D?ROD=${encodeURIComponent(CURRENT_PROJECT)}` },
-  
-    ]
-  },
-      {
-    name: "----------",
-    pages: [
-      { name: "Sadržaj", path: "/pages/KONCEPT/Navigacija" }
-    ]
-  }
-];
+  return [
+    {
+      name: `Obitelji`,
+      pages: [
+        { name: "  Prezime",      path: `/pages/1_Jularic/prezime_r?ROD=${encodeURIComponent(rod)}` },
+        { name: "  Generacije",   path: `/pages/ROD/Generacije_R?ROD=${encodeURIComponent(rod)}` },
+        { name: "  Obitelji",     path: `/pages/ROD/Obitelji_R?ROD=${encodeURIComponent(rod)}` },
+        { name: "  Događaji",     path: `/pages/ROD/Dogadjaji_R?ROD=${encodeURIComponent(rod)}` },
+        { name: "  Stablo",       path: `/pages/ROD/Stablo_R?ROD=${encodeURIComponent(rod)}` },
+        { name: "  Bolesti",      path: `/pages/ROD/Bolesti_D?ROD=${encodeURIComponent(rod)}` },
+        { name: "  Mjesta",       path: `/pages/ROD/Mjesta_R?ROD=${encodeURIComponent(rod)}` },
+        { name: "  Migracije",    path: `/pages/ROD/Migracije_R?ROD=${encodeURIComponent(rod)}` },
+        { name: "  Izvori*",      path: `/pages/ROD/Izvori_D?ROD=${encodeURIComponent(rod)}` },
+      ]
+    },
+    {
+      name: "Mjesta",
+      pages: [...mjestaPages]
+    },
+    {
+      name: "Izvori",
+      pages: [
+        { name: "  Popisi",        path: `/pages/ROD/Izvor_Popisi?ROD=${encodeURIComponent(rod)}` },
+        { name: "  Matice",        path: `/pages/ROD/Matice_D?ROD=${encodeURIComponent(rod)}` },
+        { name: "  Groblje",       path: `/pages/ROD/Groblje_D?ROD=${encodeURIComponent(rod)}` },
+        { name: "  Katastar",      path: `/pages/ROD/Katastar_D?ROD=${encodeURIComponent(rod)}` },
+        { name: "  Pismo / Jezik", path: `/pages/ROD/Pismo_D?ROD=${encodeURIComponent(rod)}` },
+      ]
+    },
+    {
+      name: "----------",
+      pages: [
+        { name: "Sadržaj", path: "/pages/KONCEPT/Navigacija" }
+      ]
+    }
+  ];
+}
