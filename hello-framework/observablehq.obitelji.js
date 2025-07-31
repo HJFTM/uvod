860f4c8d6a6713e7fb5157c4dc6951666542ad72
HJFTM <homejularic@gmail.com>
2025-07-31 14:56:02 +0200

    Update observablehq.obitelji.js

--- hello-framework/observablehq.obitelji.js
+++ hello-framework/observablehq.obitelji.js
@@ -15,20 +15,26 @@ const obiteljiBH = generirajObiteljiPoMjestu(data, "Bosna");
 const obiteljiST = generirajObiteljiPoMjestu(data, "Stupnik");
 const obiteljiDU = generirajObiteljiPoMjestu(data, "Dubrovnik");
 
-const obiteljiGEO = generirajObiteljiGEOpoMjestu(data, "Bosna");
-const obiteljiZapis = generirajObiteljiZapis(data, "Bosna");
-const obiteljiStablo = generirajObiteljiStablo(data, "Bosna");
+const obiteljiGEOBH = generirajObiteljiGEOpoMjestu(data, "Bosna");
+const obiteljiGEOST = generirajObiteljiGEOpoMjestu(data, "Stupnik");
+const obiteljiGEODU = generirajObiteljiGEOpoMjestu(data, "Dubrovnik");
+
+const obiteljiZapisBH = generirajObiteljiZapis(data, "Bosna");
+const obiteljiZapisST = generirajObiteljiZapis(data, "Stupnik");
+const obiteljiZapisDU = generirajObiteljiZapis(data, "Dubrovnik");
+
+const obiteljiStabloBH = generirajObiteljiStablo(data, "Bosna");
+const obiteljiStabloST = generirajObiteljiStablo(data, "Stupnik");
+const obiteljiStabloDU = generirajObiteljiStablo(data, "Dubrovnik");
 
 // Test
 const mjestaPages = generirajMjestaOdObitelji(data, CURRENT_PROJECT);
 
 export const obiteljiPages = [
-  ...obiteljiBH,
-  ...obiteljiST,
-  ...obiteljiDU,
-  ...obiteljiGEO,
-  ...obiteljiStablo,
-  ...obiteljiZapis,
+  ...obiteljiBH, ...obiteljiST, ...obiteljiDU,
+  ...obiteljiGEOBH, ...obiteljiGEOST, ...obiteljiGEODU,
+  ...obiteljiStabloBH, ...obiteljiStabloST, ...obiteljiStabloDU,
+  ...obiteljiZapisBH, ...obiteljiZapisST, ...obiteljiZapisDU
   
   {
     name: "------",
