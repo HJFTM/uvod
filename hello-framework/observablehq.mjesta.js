577d53138b8058001bc1677777856cadb77b2969
HJFTM <homejularic@gmail.com>
2025-08-01 14:12:57 +0200

    Update observablehq.mjesta.js

--- hello-framework/observablehq.mjesta.js
+++ hello-framework/observablehq.mjesta.js
@@ -1,1 +1,31 @@
 
+import { 
+  generirajMjestaOdObitelji,
+  generirajMjestaOdObiteljiSVE } 
+from "./observablehq.base.js";
+
+
+
+
+const obiteljiBH = generirajMjestaOdObiteljiSVE(data, "Bosna");
+const obiteljiST = generirajMjestaOdObiteljiSVE(data, "Stupnik");
+const obiteljiDU = generirajMjestaOdObiteljiSVE(data, "Dubrovnik");
+
+
+// Test
+const mjestaPages = generirajMjestaOdObitelji(data, CURRENT_PROJECT);
+
+export const obiteljiPages = [
+  ...obiteljiBH, ...obiteljiST, ...obiteljiDU,
+  ...obiteljiGEOBH, ...obiteljiGEOST, ...obiteljiGEODU,
+  ...obiteljiStabloBH, ...obiteljiStabloST, ...obiteljiStabloDU,
+  ...obiteljiZapisBH, ...obiteljiZapisST, ...obiteljiZapisDU,
+  
+  {
+    name: "------",
+    pages: [
+      { name: "Sadržaj", path: "/pages/KONCEPT/Navigacija" }
+    ]
+  }
+];
+
