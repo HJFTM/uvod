e74e7d189ca6bf7bd2c710300aa4b923fab3c4d6
HJFTM <homejularic@gmail.com>
2025-08-01 08:00:39 +0200

    Update observablehq.uvod.js

--- hello-framework/observablehq.uvod.js
+++ hello-framework/observablehq.uvod.js
@@ -43,26 +43,5 @@ export const uvodPages = [
       { name: "Individualne", path: "/pages/KONCEPT/Migracije_individualne_K" },
       { name: "Masovne",      path: "/pages/KONCEPT/Migracije_masovne_K" },
     ]
-  },
-  {
-    name: "Izvori",
-    pages: [
-      { name: "Izvori",    path: "/pages/KONCEPT/Izvori_K" },
-      { name: "Crkveni",   path: "/pages/KONCEPT/Izvori_Crkveni" },
-      { name: "Državni",   path: "/pages/KONCEPT/Izvori_Drzavni" },      
-      { name: "Gradski",   path: "/pages/KONCEPT/Izvori_Gradski" },  
-      { name: "Radovi",    path: "/pages/KONCEPT/Izvori_Radovi" },
-      { name: "Groblja",   path: "/pages/KONCEPT/Izvori_Groblja" },  
-      { name: "Pismo",            path: "/pages/KONCEPT/Pismo_K" },
-       ]
-  },
-  {
-    name: "Događaji - Zapisi",
-    pages: [   
-      { name: "Matice",           path: "/pages/KONCEPT/Izvori_zapisi_K" },
-      { name: "Kućedomaćini",     path: "/pages/KONCEPT/Kucedomacin_K" },
-      { name: "Katastar",         path: "/pages/KONCEPT/Katastar_K" },
-      { name: "Stanovnici",       path: "/pages/KONCEPT/Popis_stanovnika_K" },
-    ]
   }
 ];
