f352a6d8bb1a6c81b327788e54f88beaa9213956
HJFTM <homejularic@gmail.com>
2025-08-01 08:00:17 +0200

    Update menu.izvori.js

--- hello-framework/menu.izvori.js
+++ hello-framework/menu.izvori.js
@@ -1,1 +1,24 @@
+export const izvoriPages = [
+  {
+    name: "Izvori",
+    pages: [
+      { name: "Izvori",    path: "/pages/KONCEPT/Izvori_K" },
+      { name: "Crkveni",   path: "/pages/KONCEPT/Izvori_Crkveni" },
+      { name: "Državni",   path: "/pages/KONCEPT/Izvori_Drzavni" },      
+      { name: "Gradski",   path: "/pages/KONCEPT/Izvori_Gradski" },  
+      { name: "Radovi",    path: "/pages/KONCEPT/Izvori_Radovi" },
+      { name: "Groblja",   path: "/pages/KONCEPT/Izvori_Groblja" },  
+      { name: "Pismo",            path: "/pages/KONCEPT/Pismo_K" },
+       ]
+  },
+  {
+    name: "Događaji - Zapisi",
+    pages: [   
+      { name: "Matice",           path: "/pages/KONCEPT/Izvori_zapisi_K" },
+      { name: "Kućedomaćini",     path: "/pages/KONCEPT/Kucedomacin_K" },
+      { name: "Katastar",         path: "/pages/KONCEPT/Katastar_K" },
+      { name: "Stanovnici",       path: "/pages/KONCEPT/Popis_stanovnika_K" },
+    ]
+  }
+];
 
