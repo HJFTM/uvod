7fb297042b082a927c2a947c77515dd8a0a74273
HJFTM <homejularic@gmail.com>
2025-08-03 18:02:52 +0200

    Update observablehq.uvod.js

--- hello-framework/observablehq.uvod.js
+++ hello-framework/observablehq.uvod.js
@@ -12,14 +12,12 @@ export const uvodPages = [
       { name: "Prezime Jular-ić", path: "/pages/1_Jularic/01.1.prezime_ilaric" },
       { name: "Jularić - Ilarić", path: "/pages/1_Jularic/01.2.evolucija" },
       { name: "Ilarić",           path: "/pages/0 Uvod/3_Jularic" }, 
-      { name: "Srodna prezimena", path: "/pages/1_Prezime/prezime_srodno" },
     ]
   },
   {
     name: "Rodovi",
     pages: [
       { name: "Rodovi Ilarić",    path: "/pages/0 Uvod/4_Ilaric" },          
-      { name: "Države", path: "/pages/KONCEPT/Drzava_K" },
       { name: "Župe", path: "/pages/KONCEPT/Zupe_K" },
       { name: "Mjesta", path: "/pages/KONCEPT/Mjesta_K" },
     ]
@@ -43,5 +41,12 @@ export const uvodPages = [
       { name: "Individualne", path: "/pages/KONCEPT/Migracije_individualne_K" },
       { name: "Masovne",      path: "/pages/KONCEPT/Migracije_masovne_K" },
     ]
-  }
+  },
+  {
+    name: "Ostalo",
+    pages: [
+      { name: "Srodna prezimena", path: "/pages/1_Prezime/prezime_srodno" },
+      { name: "Države", path: "/pages/KONCEPT/Drzava_K" },
+    ]
+  },
 ];
