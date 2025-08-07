2e59b4143016eb1858be1f595320914ba7e8796c
HJFTM <homejularic@gmail.com>
2025-08-07 12:12:01 +0200

    Update observablehq.base.js

--- hello-framework/observablehq.base.js
+++ hello-framework/observablehq.base.js
@@ -23,6 +23,7 @@ if (fs.existsSync(dataPath)) {
 
 console.log("observablehq.base.js - typeof data:", typeof data);
 console.log("observablehq.base.js - Array.isArray(data):", Array.isArray(data));
+console.log("hello-framework/observablehq.config.js - data.obitelj:", data.obitelj);
 
 //⚠️ obitelji
 export { data };
