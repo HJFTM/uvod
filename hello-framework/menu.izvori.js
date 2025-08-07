9cc59135538ba089da91994faa99e8e40861153e
HJFTM <homejularic@gmail.com>
2025-08-07 08:12:35 +0200

    Update menu.izvori.js

--- hello-framework/menu.izvori.js
+++ hello-framework/menu.izvori.js
@@ -1,5 +1,6 @@
 import fs from "fs";
 import path from "path";
+import { dirname } from "path";
 
 // Ispravno lociraj JSON unutar hello-framework/src/data/
 const dataPath = path.resolve(__dirname, "src/data/dataCombined.json");
